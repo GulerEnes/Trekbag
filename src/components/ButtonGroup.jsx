71b8790629b93150b5816198db73d9ff8bba73db
enesguler <18050111005@ybu.edu.tr>
2024-04-13 18:34:20 +0300

    .

--- src/components/ButtonGroup.jsx
+++ src/components/ButtonGroup.jsx
@@ -1,13 +1,13 @@
 import Button from "./Button";
 
-function ButtonGroup() {
-	const secondaryButtons = [
-		"Mark all as complete",
-		"Mark all as incomplete",
-		"Reset to initial",
-		"Remove all items",
-	];
+const secondaryButtons = [
+	"Mark all as complete",
+	"Mark all as incomplete",
+	"Reset to initial",
+	"Remove all items",
+];
 
+function ButtonGroup() {
 	return (
 		<section className="button-group">
 			{secondaryButtons.map((text) => {
